0f80606d2c1e30d51aa87306f4a5298bd2fb375c
fernanda castillo <fersitacastillo123@gmail.com>
2016-09-28 04:58:07 +0100

    add forms and templates rot13 and user signup

--- mis-templates/main.py
+++ mis-templates/main.py
@@ -1,7 +1,10 @@
 import os
+import re
 
 import jinja2
 import webapp2
+from string import letters
+
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape= True)
@@ -18,17 +21,93 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+#Shopping List Project
 class MainPage(Handler):
     def get(self):
         items = self.request.get_all("food")
         self.render("shopping_list.html", items = items)
 
+#FizzBuzz Project, get fizz when divisible by 3, buzz by 5, fizzbuzz by both
 class FizzBuzzHandler(Handler):
     def get(self):
         n=self.request.get('n', 0)
         n = n and int(n)
         self.render('fizzbuzz.html', n=n)
 
+#Rot13 Project, change the letters for letter+13
+class RotHandler(Handler):
+    def get(self):
+        self.render('rot13.html')
+
+    def post(self):
+        rot13 = ''
+        text = self.request.get('text')
+        if text:
+            for c in text:
+                if ord(c) >= ord('a') and ord(c) <= ord('z'):
+                    rot13 += chr((ord(c) - ord('a') + 13) % 26 + ord('a'))
+                elif ord(c) >= ord('A') and ord(c) <= ord('Z'):
+                    rot13 += chr((ord(c) - ord('A') + 13) % 26 + ord('A'))
+                else:
+                    rot13 += c
+
+        self.render('rot13.html', text= rot13)
+
+#Signup project, enter valid name, password and/or email
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+class SignUpHandler(Handler):
+    def get(self):
+        self.render("signup.html")
+
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        params = dict(username = username,
+                      email = email)
+
+        if not valid_username(username):
+            params['error_username'] = "That name is not valid or too small"
+            have_error = True
+
+        if not valid_password(password):
+            params['error_password'] = "We cant accept that as a password"
+            have_error = True
+        elif password != verify:
+            params['error_verify'] = "Your password didn't match"
+            have_error = True
+
+        if not valid_email(email):
+            params['error_email'] = "That's not a valid email"
+            have_error = True
+
+        if have_error:
+            self.render('signup.html', **params)
+        else:
+            self.redirect('/welcome?username=' + username)
+
+class Welcome(Handler):
+    def get(self):
+        username = self.request.get('username')
+        if valid_username(username):
+            self.render('welcome.html', username = username)
+        else:
+            self.redirect('/signup')
+
 app = webapp2.WSGIApplication([
-    ('/', MainPage), ('/fizzbuzz', FizzBuzzHandler),
+    ('/', MainPage), ('/fizzbuzz', FizzBuzzHandler),('/rot13', RotHandler), ('/signup', SignUpHandler), ('/welcome', Welcome),
 ], debug=True)
